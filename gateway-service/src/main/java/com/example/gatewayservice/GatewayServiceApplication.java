2e27750d98a3589882578a9ac34aae84c51ec42e
dattq <trandat.00712@gmail.com>
2025-09-26 22:33:13 +0700

    Update code

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,11 +2,10 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication()
+@SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
