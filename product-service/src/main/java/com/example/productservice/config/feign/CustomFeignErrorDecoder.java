0938e69f50c6ec1a8d6208dc1eeb0818e5d3c7ad
dattq <trandat.0012@gmail.com>
2026-08-31 22:04:24 +0700

    update code

--- product-service/src/main/java/com/example/productservice/config/feign/CustomFeignErrorDecoder.java
+++ product-service/src/main/java/com/example/productservice/config/feign/CustomFeignErrorDecoder.java
@@ -1,4 +1,46 @@
 package com.example.productservice.config.feign;
 
-public class CustomFeignErrorDecoder {
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CustomFeignErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder defaultDecoder = new Default();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Exception decode(String s, Response response) {
+
+        Object exception;
+        if (Objects.nonNull(response.body())) {
+            try (InputStream inputStream = response.body().asInputStream()) {
+
+                String bodyAsString = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                        .lines().collect(Collectors.joining("\n"));
+                log.error("[FeignClient response error] - method: {} - error: {}", s, bodyAsString);
+                objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        return null;
+    }
 }
